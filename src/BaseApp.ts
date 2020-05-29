fe98b44752818233a4d236fe547c8a3394927ca0
saitejdandge <saitejdandge1@gmail.com>
2020-05-29 21:22:32 +0530

    cors

--- src/BaseApp.ts
+++ src/BaseApp.ts
@@ -35,6 +35,12 @@ export class BaseApp {
     controllers: BaseController<BasePresenter>[],
   ) {
     this.app = express();
+    this.app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      next();
+    });
     this.dbConfig = dbConfig;
     this.authConfig = authConfig;
     this.connectToTheDatabase();
